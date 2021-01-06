dcc47f7edcb6b128ec2b3d1b12deef9294047dfb
TranLe2403 <trantran.0324@gmail.com>
2021-01-06 02:41:21 +0200

    split blogList backend into separate modules

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = `mongodb+srv://phonebook:phonebook@cluster0.ud05y.mongodb.net/blogList?retryWrites=true&w=majority`;
-
-mongoose.connect(mongoUrl, {
+mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
@@ -25,21 +15,5 @@ mongoose.connect(mongoUrl, {
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use("/api/blogs", blogsRouter);
+module.exports = app;
